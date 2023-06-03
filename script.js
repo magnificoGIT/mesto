9f6fe74b07abdbc2c9769904dd097972c7bbd77c
Igor Klein <savonarola1452@yandex.ru>
2023-06-03 22:51:14 +0200

    Сделать логику открытия и закрытия попапа, а также редактирования и сохранения данных в полях для формы попапа

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-//Функционал открытия и закрытия popup
 const popup = document.querySelector('.popup');
 const profileButtonEdit = document.querySelector('.profile__button_edit');
 const popupButtonClose = popup.querySelector('.popup__button_close');
+const profile = document.querySelector('.profile');
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
 
+let popupContainer = popup.querySelector('.popup__container');
+let popupInputName = popupContainer.querySelector('.popup__input_name');
+let popupInputAboutMyself = popupContainer.querySelector('.popup__input_about-myself');
+
+//Функционал открытия и закрытия popup
 function popupOpen() {
     popup.classList.add('popup__opened');
 }
@@ -15,17 +22,24 @@ function popupClose() {
 
 popupButtonClose.addEventListener('click', popupClose);
 
-//Функционал обработчика форм
-let popupContainer = popup.querySelector('.popup__container');
-
-let popupInputName = popup.querySelector('.popup__input_name');
-
-let popupInputAboutMyself = popup.querySelector('.popup__input_about-myself');
-
-function handleFormSubmit(evt) {
-    evt.preventDefault();
-    let nameValue = popupInputName.value;
-    let aboutMyselfValue = popupInputAboutMyself.value;
+//Функционал заполненых полей при открытии popup
+function fillingFormPopup() {
+    popupInputName.value = profileTitle.textContent;
+    popupInputAboutMyself.value = profileSubtitle.textContent;
 }
 
-//Функционал редактирования текста в полях ввода
+profileButtonEdit.addEventListener('click', fillingFormPopup);
+
+//Функционал сохранения редактируемого текста  и закрытия popup на кнопку "Сохранить"
+function SavingTextFormPopup() {
+    profileTitle.textContent = popupInputName.value;
+    profileSubtitle.textContent = popupInputAboutMyself.value;
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    SavingTextFormPopup();
+    popupClose();
+}
+
+popupContainer.addEventListener('submit', handleFormSubmit); 
